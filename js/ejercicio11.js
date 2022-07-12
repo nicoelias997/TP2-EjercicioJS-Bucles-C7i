5663a2edc732a1fe96aca268db8ce53468323716
nicoelias997 <nicoelias997@gmail.com>
2022-07-12 18:24:36 +0200

    haremos ejercicios con math

---
+++ js/ejercicio11.js
@@ -0,0 +1,8 @@
+// 11- Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. *
+
+let nombre1 = prompt("Escriba su nombre");
+let edad1 = parseInt(prompt("Escriba su edad"));
+let nombre2 = prompt("Escriba su nombre");
+let edad2 = parseInt(prompt("Escriba su edad"));
+let nombre3 = prompt("Escriba su nombre");
+let edad3 = parseInt(prompt("Escriba su edad"));
