6b8d30a02d0b359ad4447e6f9834ee741b2f10be
nicoelias997 <nicoelias997@gmail.com>
2022-07-12 18:35:29 +0200

    Ejercicio resuelto sin problemas

---
+++ js/ejercicio11.js
@@ -0,0 +1,18 @@
+// 11- Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. *
+
+let nombre1 = prompt("Escriba su nombre");
+let edad1 = parseInt(prompt("Escriba su edad"));
+let nombre2 = prompt("Escriba su nombre");
+let edad2 = parseInt(prompt("Escriba su edad"));
+let nombre3 = prompt("Escriba su nombre");
+let edad3 = parseInt(prompt("Escriba su edad"));
+
+if(Math.max(edad1,edad2,edad3) == edad1){
+    document.write("El mayor es " + nombre1);
+}
+if(Math.max(edad1,edad2,edad3) == edad2){
+    document.write("El mayor es " + nombre2);
+}
+if(Math.max(edad1,edad2,edad3) == edad3){
+    document.write("El mayor es " + nombre3);
+}
